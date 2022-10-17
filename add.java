5059341a509d28d3898bb7daa5f6633f3b4413a2
vjcet <100302520+vjcet@users.noreply.github.com>
2022-10-17 22:11:06 +0530

    Create add.java
    add

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int x = 89; 
+        
+        int y = 43;
+        
+        int sum = x + y;
+        
+        System.out.println("Sum is " + sum);
+    }
+}
