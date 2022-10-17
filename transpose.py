64c6a9b886613d56cd517ae663c363d3bc3d3dc0
Abhirami123-CS <114503919+Abhirami123-CS@users.noreply.github.com>
2022-10-17 16:19:58 +0530

    Create transpose.py

---
+++ transpose.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(f'Original Array:\n{arr1}')
+
+arr1_transpose = arr1.transpose()
+
+print(f'Transposed Array:\n{arr1_transpose}')
