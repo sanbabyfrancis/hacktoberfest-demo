4dd377bcd7cda6c4f1f34a86ae889b91f35d620e
JacobGeorge2001 <85425681+JacobGeorge2001@users.noreply.github.com>
2022-10-17 14:57:21 +0530

    Create stringindex.py

---
+++ stringindex.py
@@ -0,0 +1,7 @@
+sentence = 'Python programming is fun.'
+
+result = sentence.index('is fun')
+print("Substring 'is fun':", result)
+
+result = sentence.index('Java')
+print("Substring 'Java':", result)
