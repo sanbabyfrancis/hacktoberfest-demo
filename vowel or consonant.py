1f4b93bcc9c83636d0c68f2e6024669447ea591d
alengeorge9526 <85437635+alengeorge9526@users.noreply.github.com>
2022-10-17 14:31:45 +0530

    Create vowel or consonant.py

---
+++ vowel or consonant.py
@@ -0,0 +1,12 @@
+
+def vowelOrConsonant(x):
+
+	if (x == 'a' or x == 'e' or
+		x == 'i' or x == 'o' or x == 'u'):
+		print("Vowel")
+	else:
+		print("Consonant")
+vowelOrConsonant('c')
+vowelOrConsonant('e')
+	
+
