6835995fc2d0c24264df15eb24e67698233e4224
Krishnadev P Melevila <krishnadev@kali>
2022-10-24 22:45:51 +0530

    Singly Linked List Node Creation in c

--- singlyLinkedListNodeCreation.c
+++ singlyLinkedListNodeCreation.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
-struct node{
+struct node
+{
     struct node *link;
     int data;
 };
-void main(){
+struct node *head, *newNode;
+void create()
+{
+    int item;
+    head = (struct node *)malloc(sizeof(struct node));
+    printf("Enter element");
+    scanf("%d", &item);
+    //  create new node
+    newNode->data = item;
+    newNode->link = head;
+    head = newNode;
+}
+void main()
+{
     struct node *ptr;
-    ptr = (struct node *) malloc(sizeof(struct node));
+    ptr = (struct node *)malloc(sizeof(struct node));
 }
